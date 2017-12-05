a9705fa18bd64fe5cbeea0361caf0397703ba926
deskinsjt <jtd55@case.edu>
2017-12-05 15:30:41 -0500

    Fixed some silly bugs on things I didn't test. And made it easier to not use 'visa' when testing.

--- lecroy_keyboard.py
+++ lecroy_keyboard.py
@@ -1,38 +1,51 @@
-from pynput import keyboard
+import pynput.keyboard as pnk
 import visa
 
-def on_press(key):
-    # print("Hit: '{}'".format(parse_input(key)))
-    k1.press(key)
-    k2.press(key)
-
-def on_release(key):
-    # print("Released: '{}'".format(parse_input(key)))
-    k1.release(key)
-    k2.release(key)
-    if key == keyboard.Key.esc:
-        return False
-
 class Keyboard:
-    def __init__(self, keys, key_mod_pair, channel, speaker):
+    def __init__(self, keys, key_mod_pair, channel, write_command = print, frq = 55):
         self.keys = keys
         self.modifier_keys = dict(tuple(k) for k in key_mod_pair)
         self.modifier_state = {k: False for k in self.modifier_keys.values()}
         self.pressed_keys = ""
-        self.current_note = {'keys':"", 'mods':self.modifier_state}
+        self.current_note = {'keys':"", 'mods':self.modifier_state.copy()}
         self.channel = channel
-        self.speaker = speaker
+        self.write_command = write_command
+        self.base_frq = frq
 
-    def parse_input(self,key):
+    @classmethod
+    def parse_input(cls,key):
         try:
             return key.char.lower()
         except AttributeError:
             return str(key)[4:]
 
+    @classmethod
+    def get_write_command(cls):
+        print("Gathering available devices...")
+        # visa.log_to_screen()
+        ports=visa.ResourceManager()
+        val == None
+        while val == None:
+            i=0
+            print("Choose device to connect to:")
+            for port in ports.list_resources():
+                print('\t'+ i + '. ' + str(port))
+                i = i + 1
+            try:
+                val = int(input())
+            except:
+                print("Not an integer.")
+            if val > len(ports.list_resources())-1:
+                print("Not an available device.")
+                val = None
+        write_command = ports.open_resource(ports.list_resources()[val]).write
+        print("Connected to: " + ports.list_resources()[val])
+        return write_command
+
     def release(self, key):
         released = self.parse_input(key)
         self.pressed_keys = self.pressed_keys.replace(released,"")
-        if released in self.modifier_keys.keys:
+        if released in self.modifier_keys:
             self.modifier_state[self.modifier_keys[released]] = False
         self.play_note()
 
@@ -40,69 +53,63 @@ class Keyboard:
         pressed = self.parse_input(key)
         if (pressed in self.keys) and (pressed not in self.pressed_keys):
             self.pressed_keys += pressed
-        if pressed in self.modifiers_keys.keys:
+        if pressed in self.modifier_keys:
             self.modifier_state[self.modifier_keys[pressed]] = True
         self.play_note()
 
     def play_note(self):
         try:
-            next_key = pressed_keys[-1]
+            next_key = self.pressed_keys[-1]
         except IndexError:
-            next_key = ""
-        next_note['keys'] = next_key
-        next_note['mods'] = self.modifier_state
+            next_key = ''
+        next_note = {'keys': next_key, 'mods': self.modifier_state.copy()}
 
-        if next_note != self.curent_note:
+        if next_note != self.current_note:
             self.current_note = next_note
             self.make_noise()
 
     def make_noise(self):
         frq = self.current_frq()
-        self.speaker.write(self.channel+':outp on')
         if frq == None:
             command = self.channel + ':outp off'
         else:
-            command=self.channel + ':bswv frq,{:.3f}hz'.format(frq)
-        self.speaker.write(command)
+            command = self.channel+':outp on;'
+            command += self.channel + ':bswv frq,{:.3f}hz'.format(frq)
+        self.write_command(command)
 
     def current_frq(self):
         note = list(self.current_note['keys'])
         if note == []:
             return None
 
-        return ( self.base_frq*2**(note[-1]/len(self.keys))
+        return ( self.base_frq*2**(self.keys.index(note[-1])/len(self.keys))
             *2**(self.modifier_state.get('octave',0)) )
 
 
-print("Program started.")
-print("Gathering available devices...")
-ports=visa.ResourceManager()
-val == None
-while val == None
-    i=0
-    print("Connect to:")
-    for port in ports.list_resources():
-        print('\t'+ i + '. ' + str(port))
-        i++
-    try:
-        val = int(input())
-    except:
-        print("Not an integer.")
-    if val > len(ports.list_resources())-1
-        print("Not an available device.")
-        val = None
+def on_press(key,k1,k2):
+    # print("Pressed: '{}'".format(Keyboard.parse_input(key)))
+    k1.press(key)
+    k2.press(key)
 
-connection = ports.open_resource(ports.list_resources()[val])
-print("Connected to: " + ports.list_resources()[val])
+def on_release(key,k1,k2):
+    if key == pnk.Key.esc:
+        return False
+    # print("Released: '{}'".format(Keyboard.parse_input(key)))
+    k1.release(key)
+    k2.release(key)
 
+print("N.B.-- This program intercepts all key strokes while running.")
+print("\tBe wise what you type while this is running")
+# write_command = Keyboard.get_write_command()
 print("Creating keyboards...")
-k1 = Keyboard("qwerasdfzxcv", [["shift","octave"]], "C1", connection)
+k1 = Keyboard("qwerasdfzxcv", [("shift","octave")], "C1")
 
-k2 = Keyboard("uiopjk;lm,./", [["shift_r","octave"]], "C2", connection)
+k2 = Keyboard("uiopjk;lm,./", [("shift_r","octave")], "C2")
 
 print("Ready to play! Hit 'esc' to stop.")
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+
+# Collect events until terminated
+with pnk.Listener(
+        on_press=lambda key: on_press(key,k1,k2),
+        on_release=lambda key: on_release(key,k1,k2)) as listener:
     listener.join()
