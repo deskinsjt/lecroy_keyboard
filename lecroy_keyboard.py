cc91f814564e2151f15bbe99820beaf323b65dbe
deskinsjt <jtd55@case.edu>
2017-12-06 20:45:09 -0500

    Brushed up listener class and made a speaker class.

--- lecroy_keyboard.py
+++ lecroy_keyboard.py
@@ -1,20 +1,53 @@
 class Keyboard:
-    def __init__(self, keys, key_mod, channel, write_command=print, frq=55):
+    def __init__(self, keys, key_mod, play=print, frq=55):
         self.keys = keys
         self.modifier_keys = dict(tuple(k) for k in key_mod)
         self.modifier_state = {k: False for k in self.modifier_keys.values()}
         self.pressed_keys = ""
         self.current_note = {'keys':"", 'mods':self.modifier_state.copy()}
-        self.channel = channel
-        self.write_command = write_command
+        self.play = play
         self.base_frq = frq
+        self.step = 12
 
-    @classmethod
-    def parse_input(cls,key):
+    def release(self, key):
+        self.pressed_keys = self.pressed_keys.replace(key,"")
+        if key in self.modifier_keys:
+            self.modifier_state[self.modifier_keys[key]] = False
+        self.play_note()
+
+    def press(self, key):
+        if (key in self.keys) and (key not in self.pressed_keys):
+            self.pressed_keys += key
+        if key in self.modifier_keys:
+            self.modifier_state[self.modifier_keys[key]] = True
+        self.play_note()
+
+    def play_note(self):
         try:
-            return key.char.lower()
-        except AttributeError:
-            return str(key)[4:]
+            next_key = self.pressed_keys[-1]
+        except IndexError:
+            next_key = ''
+            if self.modifier_state.get('sustain',False):
+                next_key = self.current_note['keys']
+        next_note = {'keys': next_key, 'mods': self.modifier_state.copy()}
+
+        if next_note != self.current_note:
+            self.current_note = next_note
+            self.play(self.current_frq())
+
+    def current_frq(self):
+        note = list(self.current_note['keys'])
+        if note == []:
+            return None
+
+        return ( self.base_frq*2**(self.keys.index(note[-1])/self.step)
+            *2**(self.modifier_state.get('octave',0)) )
+
+
+class LecroySpeaker:
+    def __init__(self):
+        self.write = LecroySpeaker.get_write_command()
+        self.amp = self.write(':bswv amp?')
 
     @classmethod
     def get_write_command(cls):
@@ -27,7 +60,7 @@ class Keyboard:
 
         # visa.log_to_screen()
         ports=visa.ResourceManager()
-        avail_ports = ['No device; use `print`']+list(ports.list_resources())
+        avail_ports = ['No device; use `print`'] + list(ports.list_resources())
         val = None
         while val == None:
             i=0
@@ -53,94 +86,73 @@ class Keyboard:
 
         return write_command
 
-    def release(self, key):
-        released = self.parse_input(key)
-        self.pressed_keys = self.pressed_keys.replace(released,"")
-        if released in self.modifier_keys:
-            self.modifier_state[self.modifier_keys[released]] = False
-        self.play_note()
-
-    def press(self, key):
-        pressed = self.parse_input(key)
-        if (pressed in self.keys) and (pressed not in self.pressed_keys):
-            self.pressed_keys += pressed
-        if pressed in self.modifier_keys:
-            self.modifier_state[self.modifier_keys[pressed]] = True
-        self.play_note()
-
-    def play_note(self):
-        try:
-            next_key = self.pressed_keys[-1]
-        except IndexError:
-            next_key = ''
-        next_note = {'keys': next_key, 'mods': self.modifier_state.copy()}
-
-        if next_note != self.current_note:
-            self.current_note = next_note
-            self.make_noise()
-
-    def make_noise(self):
-        frq = self.current_frq()
-        if frq == None:
-            command = self.channel + ':outp off'
-        else:
-            command = self.channel + ':outp on;'
-            command += self.channel + ':bswv frq,{:.3f}hz'.format(frq)
-        self.write_command(command)
-
-    def current_frq(self):
-        note = list(self.current_note['keys'])
-        if note == []:
-            return None
-
-        return ( self.base_frq*2**(self.keys.index(note[-1])/len(self.keys))
-            *2**(self.modifier_state.get('octave',0)) )
+    def play(self,channel):
+        def play_internal(frq):
+            if frq == None:
+                command = channel + ':bswv amp,4mv'
+                # command = self.channel + ':outp off'
+            else:
+                command = channel + ':bswv frq,{:.3f}hz;'.format(frq)
+                command += channel + ':bswv amp,{}'.format(self.amp)
+                # command += self.channel + ':outp on;'
+            self.write(command)
+        return play_internal
 
 
 class KeyboardListener:
     def __init__(self,*args):
         self.keyboards = args
+        try:
+            self.pnk = __import__('pynput').keyboard
+        except Exception as e:
+            print("Failed to import `pynput.keyboard`")
+            raise e
+
+    @classmethod
+    def parse_input(cls,key):
+        try:
+            return key.char.lower()
+        except AttributeError:
+            return str(key)[4:]
 
     def on_press(self):
         def internal_on_press(key):
-            # print("Pressed: '{}'".format(Keyboard.parse_input(key)))
+            # print("Pressed: {0} '{1}''".format(key,self.parse_input(key)))
             for k in self.keyboards:
-                k.press(key)
+                k.press(self.parse_input(key))
         return internal_on_press
 
     def on_release(self):
         def internal_on_release(key):
-            if key == pnk.Key.esc:
+            if key == self.pnk.Key.esc:
                 return False
-            # print("Released: '{}'".format(Keyboard.parse_input(key)))
+            # print("Released: {0} '{1}''".format(key,self.parse_input(key)))
             for k in self.keyboards:
-                k.release(key)
+                k.release(self.parse_input(key))
         return internal_on_release
 
     def listen(self):
-        try:
-            import pynput.keyboard
-        except Exception as e:
-            print("Failed to import `pynput.keyboard`")
-            raise e
+
         print("N.B.-- This program intercepts all key strokes while running.")
         print("\tBe wise what you type while this is running")
         print("Ready to play! Hit 'esc' to stop.")
-        with pynput.keyboard.Listener(
+        with self.pnk.Listener(
                 on_press=self.on_press(),
                 on_release=self.on_release()) as listener:
             listener.join()
             # Collect events until terminated
 
 
+
 def main():
-    write_command = Keyboard.get_write_command()
+
+    print("Connecting Speakers...")
+    s1 = LecroySpeaker()
 
     print("Creating keyboards...")
+    k1 = Keyboard("=-0987654321", [("up","octave"),("down","sustain")], s1.play("c1"), 110)
 
-    k1 = Keyboard("0987654321`", [("shift","octave")], "c1", write_command,110)
-
-    k2 = Keyboard("poiuytrewq]", [("shift_r","octave")], "c2", write_command,110*2**(5/12))
+    k2 = Keyboard("][poiuytrewq", [("up","octave"),("down","sustain")], s1.play("c2"), 110*2**(7/12))
 
     kl = KeyboardListener(k1,k2)
 
